1e25c26f71898edf84ff1d7a5a286dafe2ae4a63
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-05-28 13:21:57 +0200

    Added AddSni functionality

---
+++ Yoyyin.Prevalence.Tests/Users/Commands/AddSniFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Yoyyin.Model.Users.AggregateRoots;
+using Yoyyin.Model.Users.Commands;
+using Yoyyin.Model.Users.ValueObjects;
+
+namespace Yoyyin.Model.Tests.Users.Commands
+{
+    [TestFixture]
+    public class AddSniFixture : SetupFixture
+    {
+        [SetUp]
+        public void Setup()
+        {
+            base.Setup();
+        }
+
+        [Test]
+        public void Add()
+        {
+            Assert.That(UserRepository.Query(model => model.Snis).Count(), Is.EqualTo(0));
+
+            UserRepository.Execute(
+                new AddSniCommand(new Sni
+                                      {
+                                          SniHead = new SniHead {SniHeadId = "1", Title = "Ett"},
+                                          SniItem = new SniItem {SniNo = "A", Title = "Title"}
+                                      }));
+
+            Assert.That(UserRepository.Query(model => model.Snis).Count(), Is.EqualTo(1));
+        }
+    }
+}
---
+++ Yoyyin.Prevalence/Q&A/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Yoyyin.Model.Users.AggregateRoots;
+
+namespace Yoyyin.Model.Users
+{
+    public class QandAModel
+    {
+        // Basedata should be the only serialized datamember since the rest are calculated or application state dependent
+        //[DataMember]
+        //public BaseData BaseData { get; set; }
+
+        public QandAModel()
+        {
+          //  BaseData = new BaseData();
+            Invalidate();
+
+            Questions = new List<Question>();
+        }
+
+        // indexes
+        public ILookup<Guid, Question> QuestionsByUser { get; set; }
+
+        public List<Question> Questions { get; set; }
+
+
+        public void Invalidate()
+        {
+            //UsersByCategory = Users.ToLookup(u => u.Category.CategoryId);
+            //QuestionsByUser = Questions.ToLookup(q => q.UserId);
+        }
+    }
+}

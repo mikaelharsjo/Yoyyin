67893068c6248bbcc0c86d5a91c6b86e71616430
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-16 22:45:24 +0200

    Added ListBySniHead

---
+++ Yoyyin.Mvc/Controllers/SniController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Yoyyin.Model.Users;
+using Yoyyin.Model.Users.AggregateRoots;
+using Yoyyin.Model.Users.ValueObjects;
+
+namespace Yoyyin.Mvc.Controllers
+{
+    public class SniController : Controller
+    {
+        private IUserRepository _userRepository;
+
+        public SniController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public ActionResult ListHead()
+        {
+            return
+                View(
+                    _userRepository.Query(m => m.Snis).GroupBy(k => k.SniHead.SniHeadId).Select(
+                        g => g.First(s => s.SniHead.SniHeadId == g.Key)).ToList());
+        }
+    }
+}

cdd8d036f6a7869e1b1edac59f8849330a38c72e
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-18 13:14:13 +0200

    Created view fro ListSniHead

---
+++ Yoyyin.Mvc/Models/Sni.cs
@@ -0,0 +1,9 @@
+namespace Yoyyin.Mvc.Models
+{
+    public class Sni
+    {
+        public string Title { get; set; }
+        public string UrlToIdeas { get; set; }
+        public string UrlToItems { get; set; }
+    }
+}

07bb794a08b31357cc1c94ff948390f4d8905e9f
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-19 13:02:40 +0200

    Added new controller

---
+++ Yoyyin.Model/SearchWords/SearchWordsOrganizer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Yoyyin.Model.SearchWords
+{
+    public class SearchWordsOrganizer
+    {
+        //private readonly ISearchWordsProvider _searchWordsProvider;
+        private readonly Dictionary<string, int> _searchWordsWithCount;
+        
+        //public SearchWordsOrganizer(ISearchWordsProvider searchWordsProvider)
+        //{
+        //    _searchWordsProvider = searchWordsProvider;
+        //    _searchWordsWithCount = new Dictionary<string, int>();
+
+        //    OrganizeSearchWords();
+        //}
+
+        private void OrganizeSearchWords()
+        {
+            //foreach(string input in _searchWordsProvider.GetWords())
+            //{
+            //    foreach(string item in input.Split(','))
+            //    {
+            //        string trimmed = item.Trim().ToLower();
+            //        if (_searchWordsWithCount.ContainsKey(trimmed))
+            //            _searchWordsWithCount[trimmed]++;
+            //        else
+            //            _searchWordsWithCount.Add(trimmed, 1);
+            //    }
+            //}
+        }
+
+        /// <summary>
+        /// Should return searchwords without duplicates, sorted by weight. Used by auto complete for competences
+        /// </summary>
+        public IEnumerable<string> GetAllSearchWords()
+        {
+            var myList = new List<KeyValuePair<string, int>>(_searchWordsWithCount);
+            
+            // sort by key=weight, descending
+            myList.Sort((firstPair, nextPair) => firstPair.Value.CompareTo(nextPair.Value));
+            
+            return myList.Select(x => x.Key)
+                .Reverse();
+        }
+    }
+}
---
+++ Yoyyin.Mvc/Controllers/MatchingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Yoyyin.Model.Users;
+
+namespace Yoyyin.Mvc.Controllers
+{
+    public class MatchingController : Controller
+    {
+        private IUserRepository _userRepository;
+
+        public MatchingController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public ActionResult GetQuickSearchTypeAheadItems()
+        {
+            // TODO: move to class
+            List<string> words = new List<string>();
+            var competences = _userRepository
+                .Query(m => m.Users.Select(u => u.Ideas.First().SearchProfile.Competences));
+
+            var searchWords = _userRepository
+                .Query(m => m.Users.Select(u => u.Ideas.First().SearchProfile.SearchWords));
+
+            var concatenated = competences.Concat(searchWords);
+
+            foreach (var array in concatenated)
+            {
+                foreach (string word in array)
+                {
+                    words.Add(word);
+                }
+            }
+
+            return Json(words, JsonRequestBehavior.AllowGet);
+        }
+    }
+}

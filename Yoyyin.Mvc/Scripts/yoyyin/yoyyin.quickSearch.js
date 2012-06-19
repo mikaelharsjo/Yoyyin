89b4b3468cd3d39d712e02936645af4b3f8f4c91
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-19 13:02:20 +0200

    Added basic TypeAhead

--- Yoyyin.Mvc/Scripts/yoyyin/yoyyin.quickSearch.js
+++ Yoyyin.Mvc/Scripts/yoyyin/yoyyin.quickSearch.js
@@ -6,9 +6,13 @@
         _init: function () {
             var that = this;
             var $input = this.element.find("input");
-            $input.typeahead({
-                source: ["apa", "kossa"]
+
+            $.get("/Matching/GetQuickSearchTypeAheadItems/", function(items) {
+                $input.typeahead({
+                    source: items
+                });
             });
+
             $input.keydown(function (event) {
                 if (event.keyCode == 13) {
                     $.get("/User/QuickSearch/", { term: $(this).val() }, function (result) {

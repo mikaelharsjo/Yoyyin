29e8ba7bb2190f7a0fc65511facf2af233c62381
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-22 23:18:43 +0200

    Removed unused scripts

---
+++ Yoyyin.Mvc/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Yoyyin.Mvc.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //
+        // GET: /Register/
+
+        public ActionResult Step1()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Yoyyin.Mvc/Scripts/yoyyin/register.js
@@ -0,0 +1,10 @@
+﻿yoyyin.register = function () {
+    return {
+        open: function () {
+            $.get("/Register/Step1", function (html) {
+                console.log(html);
+                $("<form class='form-horizontal'>" + html + "<fieldset>").dialog({ title: "Bli medlem", modal: true, width: 800 });
+            });
+        }
+    };
+} ();

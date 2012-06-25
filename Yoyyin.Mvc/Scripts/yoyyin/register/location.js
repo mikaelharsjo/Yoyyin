103118ab98e1f394879bd7d92e6b94ccb2b85784
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-25 21:22:33 +0200

    Added location step to register

---
+++ Yoyyin.Mvc/Scripts/yoyyin/register/location.js
@@ -0,0 +1,28 @@
+﻿yoyyin.register.location = function () {
+    var getPosition = function (options) {
+        navigator.geolocation.getCurrentPosition(
+            lookupCountry,
+            null,
+            options);
+    };
+
+    var lookupCountry = function (position) {
+        console.log(position);
+//        var latlng = new google.maps.LatLng(
+//                            position.coords.latitude,
+//                            position.coords.longitude);
+
+//        var geoCoder = new google.maps.Geocoder();
+//        geoCoder.geocode({ location: latlng }, displayResults);
+    };
+
+    var displayResults = function (results, status) {
+        // here you can look through results ...
+        $("body").append("<div>").text(results[0].formatted_address);
+    };
+    return {
+        getContent: function () {
+            return getPosition();
+        }
+    };
+} ();
---
+++ Yoyyin.Mvc/Scripts/yoyyin/yoyyin.js
@@ -0,0 +1,36 @@
+﻿(function ($, location) {
+    var app = $.sammy("#sectionMainContent", function () {
+        //this.use(Sammy.Mustache, "html");
+
+        this.get('#/', function (context) {
+            context.swap("apa");
+        });
+
+        this.get("#/register/:step", function (context) {
+            var step = this.params["step"];
+
+            var buttonsMarkup = "<div class='form-actions'><button type='submit' class='btn btn-primary'>Spara</button><button class='btn'>Cancel</button></div><form>";
+            var stepPersonalMarkup = "<section><hgroup class='title'><h1>Först behöver vi lite personuppgifter.</h1></hgroup></div></section><div class='main-content'><label class='control-label' for='name'>Namn:</label><input type='text' class='input-xlarge' id='name' /><p class='help-block'>Supporting help text</p><label class='control-label' for='alias'>Alias:</label><input type='text' class='input-xlarge' id='alias' /><p class='help-block'>Supporting help text</p></div>";
+            var stepLocationMarkup = "<section><hgroup class='title'><h1>Var är du?</h1></hgroup></div></section><div class='main-content'><label class='control-label' for='street'>Gatuadress:</label><input type='text' class='input-xlarge' id='street' /><label class='control-label' for='zipCode'>Postnummer:</label><input type='text' class='input-xlarge' id='zipCode' /><label class='control-label' for='city'>Stad/ort:</label><input type='text' class='input-xlarge' id='city' /></div>";
+
+            switch (step) {
+                case "personalInfo":
+                    context.swap(stepPersonalMarkup + buttonsMarkup);
+                    break;
+                case "location":
+                    console.log(location);
+                    context.swap(location.getContent());
+                    break;
+            }
+        });
+    });
+
+    $(function () {
+        $(".registerLink").registerLink();
+        $("#formQuickSearch").quickSearch({ $placeHolder: $("#sectionMainContent") });
+        $('#carousel').carousel();
+
+        app.run("#/register/location");
+    });
+
+})(jQuery, yoyyin.register.location);

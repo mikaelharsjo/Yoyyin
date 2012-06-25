989a480ac738e0fcbcf6d278ac39c939af559c62
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-25 13:13:45 +0200

    Added sammy.js

--- Yoyyin.Mvc/Scripts/yoyyin/yoyyin.init.js
+++ Yoyyin.Mvc/Scripts/yoyyin/yoyyin.init.js
@@ -1,10 +1,32 @@
-﻿var yoyyin = {};
+﻿/// <reference path="../sammy.js" />
+var yoyyin = { };
+    
+(function($) {
+    var app = $.sammy("#sectionMainContent", function() {
+        this.use(Sammy.Mustache, "html");
+
+        this.get('#/', function(context) {
+            context.swap("apa");
+        });
+
+        this.get("#/register/:step", function(context) {
+            
+            context.swap(this.params["step"]);
+        });
+    });
+
+    $(function () {
+        $(".registerLink").registerLink();
+        $("#formQuickSearch").quickSearch({ $placeHolder: $("#sectionMainContent") });
+        $('#carousel').carousel();
+
+        app.run("#/register");
+    });
+
+})(jQuery);
+
 
 // initialize
-$(function () {
-    $(".registerLink").registerLink();
-    $("#formQuickSearch").quickSearch({ $placeHolder: $("#sectionMainContent") });
-    $('#carousel').carousel();
-});
+
 
 

9c0d0beb03bb8b50eff090a6e934803e585234a2
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-06-29 11:59:19 +0200

    Added map with marker

--- Yoyyin.Mvc/Scripts/yoyyin/register/location.js
+++ Yoyyin.Mvc/Scripts/yoyyin/register/location.js
@@ -1,5 +1,5 @@
 ﻿yoyyin.register.location = function (mustache) {
-    var stepLocationMarkup = "<section><hgroup class='title'><h1>Var är du?</h1></hgroup></div></section><div class='main-content'><label class='control-label' for='street'>Gatuadress:</label><input type='text' class='input-xlarge' id='street' value='{{Street}}' /><label class='control-label' for='zipCode'>Postnummer:</label><input type='text' class='input-xlarge' id='zipCode' value='{{ZipCode}}' /><label class='control-label' for='city'>Stad/ort:</label><input type='text' class='input-xlarge' id='city' value='{{City}}' /></div><div id='registerMap'></div>";
+    var stepLocationMarkup = "<section><hgroup class='title'><h1>Var är du?</h1></hgroup></div></section><div class='span5'><label class='control-label' for='street'>Gatuadress:</label><input type='text' class='input-xlarge' id='street' value='{{Street}}' /><label class='control-label' for='zipCode'>Postnummer:</label><input type='text' class='input-xlarge' id='zipCode' value='{{ZipCode}}' /><label class='control-label' for='city'>Stad/ort:</label><input type='text' class='input-xlarge' id='city' value='{{City}}' /></div><div id='registerMap' class='span5 thumbnail'></div>";
 
     var getPosition = function (options) {
         navigator.geolocation.getCurrentPosition(
@@ -18,18 +18,30 @@
     };
 
     var displayResults = function (results, status) {
-
+        console.log(results[0]);
         var parts = results[0].address_components;
         var location = {
             Street: parts[1].long_name + " " + parts[0].long_name,
             City: parts[2].long_name,
             Country: parts[3].long_name,
-            ZipCode: parts[4].long_name
+            ZipCode: parts[4].long_name,
+            Latitude: results[0].geometry.location.$a,
+            Longitude: results[0].geometry.location.ab
         };
 
         $("#sectionMainContent").html(mustache.render(stepLocationMarkup, location));
 
-        $("#registerMap").goMap();
+        $("#registerMap").goMap({
+            latitude: location.Latitude,
+            longitude: location.Longitude,
+            zoom: 12,
+            maptype: 'ROADMAP',
+            markers: [{
+                latitude: location.Latitude,
+                longitude: location.Longitude,
+                html: {content: "<h2>Hej</h2><p>hoppla</p>"}
+            }]
+        });
     };
 
     return {

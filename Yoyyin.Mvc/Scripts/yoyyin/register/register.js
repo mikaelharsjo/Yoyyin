e5d9a8dedb1921d66af0a15f333525e28c1e5d8e
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-07-06 12:32:02 +0200

    Added question and description functions

--- Yoyyin.Mvc/Scripts/yoyyin/register/register.js
+++ Yoyyin.Mvc/Scripts/yoyyin/register/register.js
@@ -7,23 +7,36 @@
             return step.markup + buttons;
         };
 
+        var setQuestion = function (question) {
+            $(".featured").find("h1").text(question);
+        };
+
+        var setDescription = function (description) {
+            $(".featured").find("p").text(description);
+        };
+
+
         var appRegister = $.sammy("#sectionMainContent", function () {
 
 
             this.get("#/register/:step", function (context) {
                 var step = this.params["step"];
 
-                var stepPersonalMarkup = "<section><hgroup class='title'><h1>Först behöver vi lite personuppgifter.</h1></hgroup></div></section><div class='main-content'><label class='control-label' for='name'>Namn:</label><input type='text' class='input-xlarge' id='name' /><p class='help-block'>Supporting help text</p><label class='control-label' for='alias'>Alias:</label><input type='text' class='input-xlarge' id='alias' /><p class='help-block'>Supporting help text</p></div>";
-                var stepLocationMarkup = "<section><hgroup class='title'><h1>Vilken adress ska användas för visning på karta?</h1></hgroup></div></section><div class='ui-helper-clearfix'><div class='stepLeft'><label class='control-label' for='street'>Gatuadress:</label><input type='text' class='input-xlarge' id='street' value='{{Street}}' /><label class='control-label' for='zipCode'>Postnummer:</label><input type='text' class='input-xlarge' id='zipCode' value='{{ZipCode}}' /><label class='control-label' for='city'>Stad/ort:</label><input type='text' class='input-xlarge' id='city' value='{{City}}' /><label class='control-label' for='city'>Land:</label><input type='text' class='input-xlarge' id='country' value='{{Country}}' /><label class='checkbox'><input type='checkbox'> Visa mig inte på kartan</label></div><div id='registerMap' class='stepRight thumbnail'></div></div>";
-                var stepUserTypeTemplate = "<h1>Vad är din roll?</h1>";
+                var stepPersonalMarkup = "<div><label class='control-label' for='name'>Namn:</label><input type='text' class='input-xlarge' id='name' /><p class='help-block'>Supporting help text</p><label class='control-label' for='alias'>Alias:</label><input type='text' class='input-xlarge' id='alias' /><p class='help-block'>Supporting help text</p></div>";
+                var stepLocationMarkup = "<div class='ui-helper-clearfix'><div class='stepLeft'><label class='control-label' for='street'>Gatuadress:</label><input type='text' class='input-xlarge' id='street' value='{{Street}}' /><label class='control-label' for='zipCode'>Postnummer:</label><input type='text' class='input-xlarge' id='zipCode' value='{{ZipCode}}' /><label class='control-label' for='city'>Stad/ort:</label><input type='text' class='input-xlarge' id='city' value='{{City}}' /><label class='control-label' for='city'>Land:</label><input type='text' class='input-xlarge' id='country' value='{{Country}}' /><label class='checkbox'><input type='checkbox'> Visa mig inte på kartan</label></div><div id='registerMap' class='stepRight thumbnail'></div></div>";
+                //var stepUserTypeTemplate = "<h1>Vad är din roll?</h1>";
 
                 switch (step) {
                     case "personalInfo":
+                        setQuestion("Först behöver vi lite personuppgifter");
+                        setDescription("");
+                        
                         context.swap(appendButtons({ markup: stepPersonalMarkup, previousStep: "personalInfo", nextStep: "location" }));
 
                         break;
                     case "location":
-                        $(".featured").find("h1").text("Vilken adress ska användas för visning på karta?");
+                        setQuestion("Vilken adress ska användas för visning på karta?");
+                        setDescription("Vi använder bara din address...");
                         location.getContent(function (data) {
                             var html = mustache.render(stepLocationMarkup, data);
                             context.swap(appendButtons({ markup: html, previousStep: "personalInfo", nextStep: "userType" }));
@@ -33,10 +46,9 @@
 
                         break;
                     case "userType":
-                        userType.init(function (data) {
-                            console.log(data);
-                            var html = stepUserTypeTemplate + "<div>" + data + "</div>";
-                            console.log(html);
+                        setQuestion("Vilken är din roll?");
+                        setDescription("");
+                        userType.init(function (html) {
                             context.swap(appendButtons({ markup: html, previousStep: "location", nextStep: "location" }));
                         });
 

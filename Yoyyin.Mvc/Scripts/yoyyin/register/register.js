c7eae6c532726b12d34f018b8cd99fdabe86b068
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-07-09 20:28:25 +0200

    refactored out switch

--- Yoyyin.Mvc/Scripts/yoyyin/register/register.js
+++ Yoyyin.Mvc/Scripts/yoyyin/register/register.js
@@ -18,88 +18,78 @@ yoyyin.register.location =
 
         var appRegister = $.sammy("#sectionMainContent", function () {
 
-            this.get("#/register/:step", function (context) {
+            this.get("#/register/personalInfo", function (context) {
                 var step = this.params["step"];
 
                 var stepPersonalMarkup = "<div><label class='control-label' for='name'>Namn:</label><input type='text' class='input-xlarge' id='name' /><p class='help-block'>Supporting help text</p><label class='control-label' for='alias'>Alias:</label><input type='text' class='input-xlarge' id='alias' /><p class='help-block'>Supporting help text</p></div>";
                 var stepLocationMarkup = "<div class='ui-helper-clearfix'><div class='stepLeft'><label class='control-label' for='street'>Gatuadress:</label><input type='text' class='input-xlarge' id='street' value='{{Street}}' /><label class='control-label' for='zipCode'>Postnummer:</label><input type='text' class='input-xlarge' id='zipCode' value='{{ZipCode}}' /><label class='control-label' for='city'>Stad/ort:</label><input type='text' class='input-xlarge' id='city' value='{{City}}' /><label class='control-label' for='city'>Land:</label><input type='text' class='input-xlarge' id='country' value='{{Country}}' /><label class='checkbox'><input type='checkbox'> Visa mig inte på kartan</label></div><div id='registerMap' class='stepRight thumbnail'></div></div>";
+                
+                setQuestion("Först behöver vi lite personuppgifter");
+                setDescription("");
 
-                switch (step) {
-                    case "personalInfo":
-                        setQuestion("Först behöver vi lite personuppgifter");
-                        setDescription("");
-
-                        context.swap(appendButtons({ markup: stepPersonalMarkup, previousStep: "personalInfo", nextStep: "location" }));
-
-                        break;
-                    case "location":
-                        setQuestion("Vilken adress ska användas för visning på karta?");
-                        setDescription("Vi använder bara din address...");
-                        location.getContent(function (data) {
-                            var html = mustache.render(stepLocationMarkup, data);
-                            context.swap(appendButtons({ markup: html, previousStep: "personalInfo", nextStep: "userType" }));
-                        });
-
-                        //alert("location getContent");
-
-                        break;
-                    case "userType":
-                        setQuestion("Vilken är din roll?");
-                        setDescription("");
-
-                        userType.init(function (html) {
-                            context.swap(appendButtons({ markup: html, previousStep: "location", nextStep: "userTypesNeeded" }), function () {
-
-                                $("#btnSaveUserType").click(function () {
-                                    $(this).button("loading");
-                                    userType.save();
-                                });
-                            });
-                        });
-
-                        break;
-
-                    case "userTypesNeeded":
-                        setQuestion("Vad söker du?");
-                        setDescription("");
-
-                        userTypesNeeded.init(function (html) {
-                            context.swap(appendButtons({ markup: html, previousStep: "userType", nextStep: "tags" }), function () {
-
-                                $("#btnSaveUserType").click(function () {
-                                    $(this).button("loading");
-                                    userType.save();
-                                });
-
-
-                                $(":checkbox").change(function () {
-                                    if ($(this).is(":checked")) {
-                                        $(this)
-                                            .parent()
-                                            .append($("<div><label>Lägg till valfri text: <input type='text' /></label></div>"));
-                                    }
-                                    else {
-                                        $(this)
-                                            .parent()
-                                            .find("div")
-                                            .remove();
-                                    }
-                                });
-                            });
-                        });
-
-                        break;
-
-                    case "tags":
-                        setQuestion("Kompetenser/Egenskaper");
-                        setDescription("");
-                        context.swap("<ul id='competences'><li>Tag1</li><li>Tag2</li></ul>");
-                        $.get("/Matching/GetQuickSearchTypeAheadItems/", function (items) {
-                            $("#competences").tagit({ availableTags: items });
-                        });
-                        break;
-                }
+                context.swap(appendButtons({ markup: stepPersonalMarkup, previousStep: "personalInfo", nextStep: "location" }));
             });
+
+            this.get("#/register/location", function (context) {
+                setQuestion("Vilken adress ska användas för visning på karta?");
+                setDescription("Vi använder bara din address...");
+                location.getContent(function (data) {
+                    var html = mustache.render(stepLocationMarkup, data);
+                    context.swap(appendButtons({ markup: html, previousStep: "personalInfo", nextStep: "userType" }));
+                });
+            });
+
+            this.get("#/register/userType", function (context) {                    
+                setQuestion("Vilken är din roll?");
+                setDescription("");
+
+                userType.init(function (html) {
+                    context.swap(appendButtons({ markup: html, previousStep: "location", nextStep: "userTypesNeeded" }), function () {
+
+                        $("#btnSaveUserType").click(function () {
+                            $(this).button("loading");
+                            userType.save();
+                        });
+                    });
+                });
+            });
+
+            this.get("#/register/userTypesNeeded", function (context) {
+                setQuestion("Vad söker du?");
+                setDescription("");
+                userTypesNeeded.init(function (html) {
+                    context.swap(appendButtons({ markup: html, previousStep: "userType", nextStep: "tags" }), function () {
+
+                        $("#btnSaveUserType").click(function () {
+                            $(this).button("loading");
+                            userType.save();
+                        });
+
+                        $(":checkbox").change(function () {
+                            if ($(this).is(":checked")) {
+                                $(this)
+                                    .parent()
+                                    .append($("<div><label>Lägg till valfri text: <input type='text' /></label></div>"));
+                            }
+                            else {
+                                $(this)
+                                    .parent()
+                                    .find("div")
+                                    .remove();
+                            }
+                        });
+                    });
+                });
+            });
+
+            this.get("#/register/tags", function (context) {
+                    setQuestion("Kompetenser/Egenskaper");
+                    setDescription("");
+                    context.swap("<ul id='competences'><li>Tag1</li><li>Tag2</li></ul>");
+                    $.get("/Matching/GetQuickSearchTypeAheadItems/", function (items) {
+                        $("#competences").tagit({ availableTags: items });
+                    });
+                });                       
         });
 
         $(function () {

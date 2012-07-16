7bbf6116e9b93b5f40a257fa5c261828ccd1a2a2
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-07-16 22:54:25 +0200

    Added app and view for users

--- Yoyyin.Mvc/Scripts/yoyyin/user/user.js
+++ Yoyyin.Mvc/Scripts/yoyyin/user/user.js
@@ -1,7 +1,26 @@
-﻿yoyyin.user = function () {
+﻿yoyyin.user = function (mustache) {
     var appUser = $.sammy("#sectionMainContent", function () {
         this.get("#/user/all", function (context) {
             console.log("all users");
+
+            require(["text!../../Templates/User/userRow.htm"], function (template) {
+
+                $.getJSON("/User/All", function (users) {
+                    var markup = "";
+                    $.each(users, function (index, user) {
+                        competenceTemplate = "<span class='label label-info'>{{Competence}}</span>&nbsp;";
+                        user.CompetencesMarkup = "";
+                        $.each(user.Competences, function (index, competence) {
+                            //markup += competence;
+                            user.CompetencesMarkup += mustache.to_html(competenceTemplate, { Competence: competence });
+                            console.log(competence);
+                        });
+                        markup += mustache.render(template, user);
+                    });
+                    context.swap(markup);
+                });
+
+            });
         });
     });
-} ();
+} (Mustache);

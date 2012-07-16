e9acc40601198f9ea2dbb7cc152c2847882da2c2
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-07-16 12:12:27 +0200

    Added view model

--- Yoyyin.Mvc/Models/User.cs
+++ Yoyyin.Mvc/Models/User.cs
@@ -1,33 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web;
-using Yoyyin.Model.Users.Entities;
-
 namespace Yoyyin.Mvc.Models
 {
-    public class UserWithFirstIdea
+    public class User
     {
         public string DisplayName { get; set; }
-        public Idea FirstIdea { get; set; }
         public string SmallProfileImageMarkup { get; set; }
-        public string LargeProfileImageMarkup { get; set; }
-        public string SniItemTitle { get; set; }
-        public string SniHeadTitle { get; set; }
-        public string SniNo { get; set; }
-        public string SniHeadId { get; set; } 
-        public string DetailsHref { get; set; }
-        public string UserTypesNeededMarkup { get; set; }
-        public string UserTypeMarkup { get; set; }
-        public string CompetencesNeededmarkup { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; } 
-
-        public UserWithFirstIdea(string[] snis)
-        {
-            SniHeadTitle = snis[0];
-            SniItemTitle = snis[1];
-            SniNo = snis[2];
-            SniHeadId = snis[3];
-        }
+        public string Competences { get; set; }
     }
 }

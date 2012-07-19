d6ed6371179ac2160fbd5ecdf4f5580575aebc97
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-07-19 12:08:30 +0200

    Added basic teaser

---
+++ Yoyyin.Mvc/Controllers/TeaserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Yoyyin.Mvc.Controllers
+{
+    public class TeaserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

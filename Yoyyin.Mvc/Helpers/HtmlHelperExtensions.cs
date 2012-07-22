a4dce8052efa1caf4188c7c3ff8dab2ad2aaf33e
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-07-22 22:45:18 +0200

    introduced html helpers

---
+++ Yoyyin.Mvc/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+namespace Yoyyin.Mvc.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlString PageHeader(this System.Web.Mvc.HtmlHelper html, string title, string byLine)
+        {
+            if (title == null && byLine == null) return html.Raw("");
+
+            const string template = @"<div class='page-header'>
+                                        <h1>{0}. <small>{1}</small></h1>            
+                                    </div>";
+
+            return html.Raw(string.Format(template, title, byLine));
+        }
+    }
+}

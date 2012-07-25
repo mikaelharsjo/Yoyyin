4366bf5f7b3f21e4bac37f7a97f5cc40d30d3b2f
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-07-25 22:59:13 +0200

    Worked with member menu

--- Yoyyin.Mvc/Scripts/yoyyin/member/member.js
+++ Yoyyin.Mvc/Scripts/yoyyin/member/member.js
@@ -3,8 +3,12 @@ yoyyin.register = function ($, sammy, mustache) {
     
     var appMember = $.sammy("#sectionMainContent", function () {
         this.get("#/member/matcher", function (context) {
-
             require(["text!../../Templates/Member/matcher.htm"], function (template) {
+                context.swap(mustache.render(template));
+            });
+        });
+        this.get("#/member/editProfile", function (context) {
+            require(["text!../../Templates/Member/editProfile.htm"], function (template) {
                 context.swap(mustache.render(template));
             });
         });

9a9135ccf09dac4e143ca508efde7267972f1803
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-08-29 23:01:56 +0200

    Added basic router for registration

---
+++ Yoyyin.Mvc/Scripts/yoyyin/registrationMain.js
@@ -0,0 +1,4 @@
+﻿require(["routers/registrationRouter"], function (Router) {
+    var router = new Router();
+    Backbone.history.start();
+});
---
+++ Yoyyin.Mvc/Scripts/yoyyin/routers/registrationRouter.js
@@ -0,0 +1,10 @@
+﻿define(["backbone"], function (Backbone) {
+    return Backbone.Router.extend({
+        routes: {
+            "step1": "step1"
+        },
+        step1: function () {
+            console.log("step1");
+        }
+    });
+});

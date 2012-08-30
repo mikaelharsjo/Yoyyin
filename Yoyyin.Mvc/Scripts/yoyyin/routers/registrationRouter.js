385cbe9fbb2d5269e41d90135705947c63118ada
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-08-30 22:42:30 +0200

    Refactored step 1 into view

--- Yoyyin.Mvc/Scripts/yoyyin/routers/registrationRouter.js
+++ Yoyyin.Mvc/Scripts/yoyyin/routers/registrationRouter.js
@@ -1,10 +1,12 @@
-﻿define(["backbone"], function (Backbone) {
+﻿define(["backbone", "views/registration/stepPersonalInfo"], function (Backbone, StepView) {
     return Backbone.Router.extend({
         routes: {
             "step1": "step1"
         },
         step1: function () {
             console.log("step1");
+            var view = new StepView({ el: $("#sectionMainContent") });
+            view.render();
         }
     });
 });

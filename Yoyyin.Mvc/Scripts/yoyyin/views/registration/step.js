981b95119e427bbcb55235990f5cddbb71cf7460
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-08-30 22:42:44 +0200

    Refactored step 1 into view

---
+++ Yoyyin.Mvc/Scripts/yoyyin/views/registration/step.js
@@ -0,0 +1,28 @@
+﻿define(["backbone", "mustache", "text!templates/registration/hero.htm"], function (Backbone, mustache, heroTemplate) {
+    var buttonsMarkup = "<div class='form-actions'><a class='btn' href='/#/register/{{previousStep}}'>Föregående</a><a class='btn btn-primary' href='/#/register/{{nextStep}}'>Nästa</a></div><form>";
+
+    return Backbone.View.extend({
+
+        appendButtons: function (step) {
+            var buttons = mustache.to_html(buttonsMarkup, step);
+            return step.markup + buttons;
+        },
+
+        setHero: function (params) {
+            if ($(".featured").html() == null) {
+                $("#body").prepend(mustache.render(heroTemplate, params));
+            } else {
+                this.setQuestion(params.Headline);
+                this.setDescription(params.Description);
+            }
+        },
+
+        setQuestion: function (question) {
+            $(".featured").find("h1").text(question);
+        },
+
+        setDescription: function (description) {
+            $(".featured").find("p").text(description);
+        }
+    });
+});
---
+++ Yoyyin.Mvc/Scripts/yoyyin/views/registration/stepPersonalInfo.js
@@ -0,0 +1,12 @@
+﻿define(["views/registration/step", "views/shared/tags/competences", "text!templates/registration/personalInfo.htm"], function (StepView, CompetencesView, template) {
+    return StepView.extend({
+        initialize: function () {
+            console.log("step personal");
+        },
+        render: function () {
+            this.setHero({ Headline: "Kul att du vill bli medlem, först vill vi höra om dig själv" });
+            this.$el.html(this.appendButtons({ markup: template, previousStep: "personalInfo", nextStep: "wanted" }));
+            var view = new CompetencesView({ el: $("#competences") });
+        }
+    });
+});

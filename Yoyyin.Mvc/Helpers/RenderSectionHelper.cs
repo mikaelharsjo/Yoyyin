5f01a818e98794392b3894e16795faae62ce0416
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-09-13 13:49:20 +0200

    Added rendersection with default content

---
+++ Yoyyin.Mvc/Helpers/RenderSectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace Yoyyin.Mvc.Helpers
+{
+    public static class Helpers
+    {
+        public static HelperResult RenderSection(this WebPageBase webPage,
+            string name, Func<dynamic, HelperResult> defaultContents)
+        {
+            if (webPage.IsSectionDefined(name))
+            {
+                return webPage.RenderSection(name);
+            }
+            return defaultContents(null);
+        }
+    }
+}

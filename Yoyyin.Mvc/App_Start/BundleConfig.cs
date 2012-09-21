7acce09343c97f5a241d4a65ff7132c5dc537dd3
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-09-21 14:57:32 +0200

    Made less transform recursive

--- Yoyyin.Mvc/App_Start/BundleConfig.cs
+++ Yoyyin.Mvc/App_Start/BundleConfig.cs
@@ -11,13 +11,20 @@ namespace Yoyyin.Mvc.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new Bundle("~/Scripts/bundled.js", new JsMinify()).Include(
-                // jquery
-                        "~/Scripts/lib/require-jquery.js",
-                        "~/Scripts/lib/require.js"));
+            bundles.Add(new Bundle("~/Scripts/bundled.js", new JsMinify())
+                            .Include("~/Scripts/lib/require-jquery.js")
+                            .Include("~/Scripts/lib/jquery.gomap-1.3.2.min.js")
+                            .Include("~/Scripts/lib/bootstrap.js")
+                            .Include("~/Scripts/lib/tag-it.js")
+                            .Include("~/Scripts/lib/jquery.gomap-1.3.2.min.js")
+                            .Include("~/Scripts/lib/require.js")
+                            .Include("~/Scripts/yoyyin/yoyyin.quickSearch.js")
+                            .Include("~/Scripts/yoyyin/yoyyin.init.js")
+                            .Include("~/Scripts/yoyyin/registrationMain.js"));
 
-            var lessBundle = new Bundle("~/Content/Css/bundled.css").IncludeDirectory("~/Content/Less", "*.less");
-            //lessBundle.Include("~/Content/Less/bootstrap.less");
+            var lessBundle = new Bundle("~/Content/Css/bundled.css")//  .IncludeDirectory("~/Content/Less", "*.less");
+                                .Include("~/Content/Less/yoyyin.less");
+
             lessBundle.Transforms.Add(new LessTransform());
             lessBundle.Transforms.Add(new CssMinify());
             bundles.Add(lessBundle);
--- Yoyyin.Mvc/Transform/LessTransform.cs
+++ Yoyyin.Mvc/Transform/LessTransform.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Optimization;
+using dotless.Core;
+using dotless.Core.Input;
+using dotless.Core.Loggers;
+using dotless.Core.configuration;
 
 namespace Yoyyin.Mvc.Transform
 {
-    public class LessTransform : IBundleTransform
-    {
-        public void Process(BundleContext context, BundleResponse response)
+
+        public class LessTransform : IBundleTransform
         {
-            response.Content = dotless.Core.Less.Parse(response.Content);
-            response.ContentType = "text/css";
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                DotlessConfiguration config = new DotlessConfiguration();
+                config.MinifyOutput = false;
+                config.ImportAllFilesAsLess = true;
+                config.CacheEnabled = false;
+                config.LessSource = typeof(VirtualFileReader);
+                #if DEBUG
+                config.Logger = typeof(DiagnosticsLogger);
+                #endif
+                response.Content = Less.Parse(response.Content, config);
+                response.ContentType = "text/css";
+            }
+
+            internal sealed class VirtualFileReader : IFileReader
+            {
+                public byte[] GetBinaryFileContents(string fileName)
+                {
+                    fileName = GetFullPath(fileName);
+                    return File.ReadAllBytes(fileName);
+                }
+
+                public string GetFileContents(string fileName)
+                {
+                    fileName = GetFullPath(fileName);
+                    return File.ReadAllText(fileName);
+                }
+
+                public bool DoesFileExist(string fileName)
+                {
+                    fileName = GetFullPath(fileName);
+                    return File.Exists(fileName);
+                }
+
+                private static string GetFullPath(string path)
+                {
+                    return HostingEnvironment.MapPath("~/Content/Less/" + path);
+                }
+            }
         }
-    }
 }

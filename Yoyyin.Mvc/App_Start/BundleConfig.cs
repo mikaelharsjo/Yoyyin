aee806410204e1c2c1fef46cfa32aa092563111d
Mikael Harsjo <mikaelharsjo@gmail.com>
2012-09-21 10:06:48 +0200

    Added less bundle (not finished)

---
+++ Yoyyin.Mvc/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using Yoyyin.Mvc.Transform;
+
+namespace Yoyyin.Mvc.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new Bundle("~/Scripts/bundled.js", new JsMinify()).Include(
+                // jquery
+                        "~/Scripts/lib/require-jquery.js",
+                        "~/Scripts/lib/require.js"));
+
+            var lessBundle = new Bundle("~/Content/Css/bundled.css").IncludeDirectory("~/Content/Less", "*.less");
+            //lessBundle.Include("~/Content/Less/bootstrap.less");
+            lessBundle.Transforms.Add(new LessTransform());
+            lessBundle.Transforms.Add(new CssMinify());
+            bundles.Add(lessBundle);
+        }
+    }
+}
---
+++ Yoyyin.Mvc/Transform/LessTransform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Yoyyin.Mvc.Transform
+{
+    public class LessTransform : IBundleTransform
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            response.Content = dotless.Core.Less.Parse(response.Content);
+            response.ContentType = "text/css";
+        }
+    }
+}
